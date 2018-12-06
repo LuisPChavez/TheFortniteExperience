2e2966e3a08e45a699b740985a351d27ec04c7b0
LuisPChavez <luis.p.chavez.1@gmail.com>
2018-12-05 23:39:14 -0800

    Created basic react project

---
+++ thefortniteexperience/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            src={
+              "https://cdn-images.threadless.com/threadless-media/artist_shops/shops/Midgets/products/581100/shirt-1527287626-e3674d84cdc64d468bf41b02101445ee.png?v=3&d=eyJvbmx5X21ldGEiOiBmYWxzZSwgImZvcmNlIjogZmFsc2UsICJvcHMiOiBbWyJ0cmltIiwgW2ZhbHNlLCBmYWxzZV0sIHt9XSwgWyJyZXNpemUiLCBbXSwgeyJ3aWR0aCI6IDk5Ni4wLCAiYWxsb3dfdXAiOiBmYWxzZSwgImhlaWdodCI6IDk5Ni4wfV0sIFsiY2FudmFzX2NlbnRlcmVkIiwgWzEyMDAsIDEyMDBdLCB7ImJhY2tncm91bmQiOiAiMDAwMDAwIn1dLCBbInJlc2l6ZSIsIFs4MDBdLCB7fV0sIFsiY2FudmFzX2NlbnRlcmVkIiwgWzgwMCwgODAwLCAiI2ZmZmZmZiJdLCB7fV0sIFsiZW5jb2RlIiwgWyJqcGciLCA4NV0sIHt9XV19"
+            }
+            className="App-logo"
+            alt="logo"
+          />
+          <p>Hello World, are you ready for the Fortnite Experience</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
