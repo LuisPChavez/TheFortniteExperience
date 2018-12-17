7c0af78dc0a97f099629f959b16c85a3472ef966
LuisPChavez <luis.p.chavez.1@gmail.com>
2018-12-17 12:58:56 -0800

    Created better NavBar

--- thefortniteexperience/src/components/App.js
+++ thefortniteexperience/src/components/App.js
@@ -1,32 +1,8 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import HomeIcon from "@material-ui/icons/Home";
-
-const styles = {
-  AppBar: {
-    backgroundColor: "#808080"
-  },
-  image: {
-    height: 135,
-    width: 240
-  }
-};
+import NavBar from "./NavBar";
 
 const App = () => {
-  return (
-    <AppBar style={styles.AppBar}>
-      <Toolbar>
-        <Button>
-          <HomeIcon />
-        </Button>
-        <Button variant="contained" color="primary">
-          Hello World!
-        </Button>
-      </Toolbar>
-    </AppBar>
-  );
+  return <NavBar />;
 };
 
 export default App;
