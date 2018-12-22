3c68eac395f4c8e99dd70998f95e0bbc1e322a7a
LuisPChavez <Luis.p.chavez.1@gmail.com>
2018-12-22 15:49:56 -0800

    Changed twitch clip

--- thefortniteexperience/server/client/src/components/TwitchClip.js
+++ thefortniteexperience/server/client/src/components/TwitchClip.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
-import Iframe from "react-iframe";
+import React, { Component } from 'react';
+import Iframe from 'react-iframe';
 
 class TwitchClip extends Component {
-  render() {
-    return (
-      <div style={{ marginTop: 10 }}>
-        <Iframe
-          url="https://clips.twitch.tv/embed?autoplay=false&clip=ApatheticClumsyCarrotGingerPower&muted=false"
-          width="1280px"
-          height="720px"
-        />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div style={{ marginTop: 10 }}>
+				<Iframe
+					url="https://clips.twitch.tv/embed?autoplay=false&clip=DarkQuaintEagleSmoocherZ&muted=false"
+					width="1280px"
+					height="720px"
+				/>
+			</div>
+		);
+	}
 }
 
 export default TwitchClip;
