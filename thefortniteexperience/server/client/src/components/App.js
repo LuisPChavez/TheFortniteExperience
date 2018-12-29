26d400ecf599aac022fb62fb72396b7c4ee64074
LuisPChavez <Luis.p.chavez.1@gmail.com>
2018-12-29 14:30:22 -0800

    Added Twitch API functionality and fixed a error with the components

--- thefortniteexperience/server/client/src/components/App.js
+++ thefortniteexperience/server/client/src/components/App.js
@@ -1,23 +1,23 @@
-import React from "react";
-import NavBar from "./NavBar";
-import TwitchClip from "./TwitchClip";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
+import React from 'react';
+import NavBar from './NavBar';
+import TwitchClip from './TwitchClip';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 const App = () => {
-  return (
-    <div>
-      <NavBar />
-      <Grid container spacing={24}>
-        <Grid item xs={2}>
-          <Paper item xs={3} />
-        </Grid>
-        <Grid item xs={3}>
-          <TwitchClip />
-        </Grid>
-      </Grid>
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<Grid container spacing={24}>
+				<Grid item xs={2}>
+					<Paper xs={3} />
+				</Grid>
+				<Grid item xs={3}>
+					<TwitchClip />
+				</Grid>
+			</Grid>
+		</div>
+	);
 };
 
 export default App;
--- thefortniteexperience/server/client/src/components/TwitchClip.js
+++ thefortniteexperience/server/client/src/components/TwitchClip.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import Iframe from 'react-iframe';
+import axios from 'axios';
+import keys from '../config/dev';
 
 class TwitchClip extends Component {
+	componentDidMount() {
+		this.state = [];
+		axios
+			.get('https://api.twitch.tv/helix/streams?game_id=33214', {
+				headers: {
+					'Client-Id': keys.twitchClientID
+				}
+			})
+			.then(res => {
+				console.log(res);
+			});
+	}
+
 	render() {
 		return (
 			<div style={{ marginTop: 10 }}>
 				<Iframe
-					url="https://clips.twitch.tv/embed?autoplay=false&clip=DarkQuaintEagleSmoocherZ&muted=false"
+					url="http://clips.twitch.tv/embed?autoplay=false&clip=DarkQuaintEagleSmoocherZ&muted=false"
 					width="1280px"
 					height="720px"
+					allowFullScreen
 				/>
 			</div>
 		);
